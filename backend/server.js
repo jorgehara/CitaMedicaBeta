c6110156451a103b3c4ebc43dd8ca751233a9a00
Jorge Hara Devs <88298206+jorgehara@users.noreply.github.com>
2025-06-15 08:48:30 -0300

    fix server common js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-import dotenv from 'dotenv';
+const dotenv = require('dotenv');
 
 dotenv.config();
 
